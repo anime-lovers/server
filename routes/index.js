c45c6c8643db123a45c08bf4e160ef0f5a6cb7e4
restuu <restu_u@outlook.com>
2018-05-03 21:34:53 +0700

    route post

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const {
+  findOrCreate,
+  generateJWTToken
+} = require('../controllers/indexController')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+// POST
+router.post('/', findOrCreate, generateJWTToken)
 
 module.exports = router;
