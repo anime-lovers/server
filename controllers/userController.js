83e0efb1018409affd242fad4033bea2a1c44cc2
restuu <restu_u@outlook.com>
2018-05-04 13:08:57 +0700

    verify token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,16 @@
+const jwt = require('jsonwebtoken')
+const secretKey = process.env.MY_SECRET
+
 module.exports = {
   auth(req, res, next) {
     let token = req.body.token
-    if (!token) {
+    let decoded = jwt.verify(token, secretKey)
+    if (!decoded) {
       return res.status(401).json({
         message: 'Please login first'
       })
     }
+    // let animeId = 
     res.status(200).json()
   }
 }
